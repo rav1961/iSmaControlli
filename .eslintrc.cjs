aa2e554fd49f819ee3a0d60efabe0031166d1932
rrojek <r.rojek87@gmail.com>
2023-11-27 01:39:40 +0100

    init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': ['off'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': 'error'
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{ts,tsx}', './index.html'],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          DEFAULT: '#8193B1',
+          100: '#b6c2d6',
+          400: '#8193B1',
+          500: '#3A4667',
+          700: '#12192C',
+          900: '#1F2843',
+        },
+        blue: '#2254C7',
+        yellow: '#F7B818',
+        white: '#FFFFFF',
+      },
+      fontFamily: {
+        sans: 'Poppins, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
+      backgroundImage: {
+        'box-gradient': `linear-gradient(255deg, #3A4667 0%, rgba(58, 70, 103, 0.00) 112.11%)`,
+        'nav-gradient': `linear-gradient(270deg, #2254C7 1.73%, rgba(34, 84, 199, 0.00) 96.53%)`,
+      },
+      borderWidth: {
+        1: '1px',
+      },
+    },
+  },
+  plugins: [],
+};
