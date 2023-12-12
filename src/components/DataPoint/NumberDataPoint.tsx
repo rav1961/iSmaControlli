0970c9fea0939221734a444617346e22b1349aee
rrojek <r.rojek87@gmail.com>
2023-12-12 06:28:13 +0100

    fix read/save datapoint value

--- src/components/DataPoint/NumberDataPoint.tsx
+++ src/components/DataPoint/NumberDataPoint.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import { postDataPointValue } from 'src/api/postDataPointValue';
 import type { AnalogDataWidget } from './DataPoint.type';
+import { debounce } from 'lodash';
 
 interface NumberDataPointPropsType {
   data: AnalogDataWidget;
@@ -11,12 +12,16 @@ const NumberDataPoint = ({ data }: NumberDataPointPropsType) => {
   const { id, curVal } = data;
   const [inputValue, setInputValue] = useState<number>(0);
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const editedValueRef = useRef(inputValue);
 
   const queryClient = useQueryClient();
 
   useEffect(() => {
-    !isEditing && setInputValue(curVal);
-  }, [curVal, isEditing]);
+    if (data && !isEditing) {
+      setInputValue(curVal);
+      editedValueRef.current = curVal;
+    }
+  }, [curVal, data, isEditing]);
 
   const saveNewValue = useMutation({
     mutationFn: postDataPointValue,
@@ -27,25 +32,47 @@ const NumberDataPoint = ({ data }: NumberDataPointPropsType) => {
     }
   });
 
-  const updateHadle = (e: ChangeEvent<HTMLInputElement>) => {
-    setIsEditing(true);
-
-    const val = parseFloat(e.target.value);
-
-    if (val) {
-      setInputValue(() => val);
-
+  const debouncedSave = debounce((value) => {
+    if (curVal !== value) {
       saveNewValue.mutate({
         id,
-        val
+        val: value
       });
     }
+  }, 0);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseFloat(e.target.value);
+
+    if (newValue) {
+      setInputValue(newValue);
+      editedValueRef.current = newValue;
+
+      if (!isEditing) {
+        setIsEditing(true);
+      }
+    }
+  };
+
+  const handleFocus = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+    debouncedSave(editedValueRef.current);
   };
 
   return (
     <div className="point-item__analog">
       <h4 className="point-item__title">{data.dis}</h4>
-      <input type="number" value={inputValue} onChange={updateHadle} />
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
     </div>
   );
 };
